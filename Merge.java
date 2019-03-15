2ff3a3e6db1d5df9390b493246b08dceb34d02f7
almathaler <athaler00@stuy.edu>
2019-03-15 11:02:26 -0400

    done in class

---
+++ Merge.java
@@ -0,0 +1,12 @@
+public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){};
+
+  private static void mergesortHelp(int[] data, int hi, int lo){
+    int seperate = (hi + lo) / 2; //if range is even, index will be one closer to start than end
+    //use copy of range for new array
+    int[] left = copyOfRange(data, lo, seperate);
+  }
+
+
+}
