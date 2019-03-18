ec58d9a202f3210071028bac9d1dbfe216b89fed
almathaler <athaler00@stuy.edu>
2019-03-17 20:05:43 -0400

    optimized underway

--- Merge.java
+++ Merge.java
@@ -1,9 +1,43 @@
 import java.util.Arrays;
 public class Merge{
-  public static void main(String[] args){
-    int[] data = {3, 2, 1, 5, 4, 6, 3, 9, 0};
-    mergesort(data);
-    //System.out.println("Now your data: " + Arrays.toString(data));
+  public static void main(String[]args){
+    int[] data = {3, 4, 2, 5, 1, 8 , 6, 5, 9, 10};
+    mergesortOpt(data);
+    System.out.println("Old data: {3, 4, 2, 5, 1, 8 , 6, 5, 9, 10}" + "\t new data: " + Arrays.toString(data));
+    /*
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          mergesort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
+    */
   }
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
@@ -65,6 +99,53 @@ public class Merge{
               i++;
             }
           } //this will occur again and again in the loop until u fill up data, will happen at same time as u run out of left values
+      }
+    }
+  }
+  //optimized mergesort
+  public static void mergesortOpt(int[] data){
+    int[] temp = Arrays.copyOf(data, data.length);
+    mergesortOH(data, temp, 0, data.length - 1);
+  }
+  private static void mergesortOH(int[]data, int[]temp, int lo, int hi){
+    if (lo < hi){
+      System.out.println("Entered a new mOH call w lo: " + lo + " and hi: " + hi);
+      int pivot;
+      if ((hi-lo) % 2 == 0){
+        pivot = ((hi+lo) / 2); // so in array of size 4, pivot will be index 2
+      }else{
+        pivot = ((hi+lo) / 2) + 1; // in array of size 5, pivot witll be index 3 (right will be shorter than left by 1)
+      }
+      System.out.println("The pivot: " + pivot);
+      System.out.println("Will mergesort sections " + lo + " to " + (pivot-1) + " and " + pivot +  " to " + hi);
+      //
+      mergesortOH(temp, data, lo, pivot-1);
+      mergesortOH(temp, data, pivot, hi); //right side will be +1 bigger than left is array is odd
+      //
+      int c = lo; //fill later, keep below hi of this call
+      int i = lo; //from i - k and k - hi have been sorted
+      int k = pivot; //read up on both sections simultaneously
+      while (c<hi){
+        // do same check as normal mergesort however the catch is like this:
+        // if i=pivot, fill rest of data up until hi w k values
+        // if k = hi, fill rest of data up with i values until pivot
+        if (k == hi){ //should always be writing into what is considered data
+          data[c] = temp[i];
+          i++;
+          c++;
+        }else if (i == pivot){
+          data[c] = temp[k];
+          k++;
+          c++;
+        }else if (temp[k] < temp[i]){
+          data[c] = temp[k];
+          k++;
+          c++;
+        }else {
+          data[c] = temp[i];
+          i++;
+          c++;
+        }
       }
     }
   }
